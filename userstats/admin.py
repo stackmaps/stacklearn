e751c951fa9e5d2a674d0b3f3fc7164a10665371
CaseyManning <kcgrind@gmail.com>
2018-01-05 10:33:40 -0800

    initial commit

---
+++ userstats/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import StudentStatistic#, Subject
+
+admin.site.register(StudentStatistic)
+# admin.site.register(Subject)
+# Register your models here.
